bc0015d4bee3e07bed98272bc0ee75cbac7e90c1
fitsumbinyam23-debug <fitsumbinyam23@gmail.com>
2026-07-11 13:59:43 +0400

    Add switch administration and stack training

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 "use strict";
 
-const CACHE_NAME = "command-doctor-2026-07-curriculum-3";
+const CACHE_NAME = "command-doctor-2026-07-admin-4";
 const OFFLINE_ASSETS = [
   "./",
   "./index.html",
   "./refresh.html",
   "./sw-refresh.js",
-  "./styles.css?v=2026.07-curriculum.3",
-  "./src/app.js?v=2026.07-curriculum.3",
-  "./src/lab-engine.js?v=2026.07-curriculum.3",
+  "./styles.css?v=2026.07-admin.4",
+  "./src/app.js?v=2026.07-admin.4",
+  "./src/lab-engine.js?v=2026.07-admin.4",
   "./data/commands/admin_commands.json",
   "./data/commands/aruba_cx.json",
   "./data/commands/cisco_ios.json",
